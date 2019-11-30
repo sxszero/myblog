f48b68126c8e519159ac9a88523b06a4883e392f
kl150666 <1075424437@qq.com>
2019-11-30 19:08:24 +0800

    实体类

---
+++ src/main/java/com/sxsduki/blog/pojo/Comment.java
@@ -0,0 +1,88 @@
+package com.sxsduki.blog.pojo;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ *
+ */
+@Entity
+@Table(name = "t_comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nickname;
+    private String email;
+    //评论内容
+    private String content;
+    //头像
+    private String avatar;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    public Comment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", email='" + email + '\'' +
+                ", content='" + content + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
