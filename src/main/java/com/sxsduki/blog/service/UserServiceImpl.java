d7ca2dc94f498b3bcca879929af529e6efdc62cc
kl150666 <1075424437@qq.com>
2019-12-01 16:22:47 +0800

    新增拦截器

--- src/main/java/com/sxsduki/blog/service/UserServiceImpl.java
+++ src/main/java/com/sxsduki/blog/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.sxsduki.blog.service;
 
 import com.sxsduki.blog.dao.UserRepository;
 import com.sxsduki.blog.pojo.User;
+import com.sxsduki.blog.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User checkUser(String username, String password) {
 
-        User user = userRepository.findByUsernameAndPassword(username,password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
 
         return user;
     }
